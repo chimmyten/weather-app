ff1ec7164fa660870f4e492fe6b61bcf53c59c89
Timmy Chen <chenx343@gmail.com>
2023-09-09 19:59:45 -0400

    change background attachment to fixed

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -1,7 +1,7 @@
 export default function WeatherInfo() {
   return (
     <div className="container">
-      <div className="row gx-5">
+      <div className="row gx-5 mt-5">
         <div className="col container stat-container">
           <div className="row justify-content-center">Chance of Rain</div>
           <div className="row justify-content-center">30%</div>
