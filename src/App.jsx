ad04894463e85f626a89b2d61727e118a45d3bad
Timmy Chen <chenx343@gmail.com>
2023-09-17 19:52:35 -0400

    move fetchData to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,48 @@
-import './App.css';
-import Header from './components/Header.jsx'
-import TempDash from './components/TempDash.jsx'
-import WeatherInfo from './components/WeatherInfo.jsx'
-import Forecast from './components/Forecast';
+import "./App.css";
+import { useState, useEffect } from "react";
+import Header from "./components/Header.jsx";
+import TempDash from "./components/TempDash.jsx";
+import WeatherInfo from "./components/WeatherInfo.jsx";
+import Forecast from "./components/Forecast";
+import { dividerClasses } from "@mui/material";
 
 function App() {
+  const API_KEY = import.meta.env.VITE_API_KEY;
+  const [weatherData, setWeatherData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=Edison&aqi=no`);
+      const data = await response.json();
+      setWeatherData(data);
+      setIsLoading(false);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <Header />
-      <div className="container app-content-container mt-5">
-        <div className="row justify-content-center">
-          <div className="col-md-6 col-lg-5 ps-5">
-            <TempDash />
+      {isLoading ? (
+        <div>
+          <h1 className="text-center mt-5">Loading...</h1>
+        </div>
+      ) : (
+        <div className="container app-content-container mt-5">
+          <div className="row justify-content-center">
+            <div className="col-md-6 col-lg-5 ps-5">
+              <TempDash weatherData={weatherData} />
+            </div>
+            <div className="col-md-6 col-lg-5 row align-items-center">
+              <WeatherInfo />
+            </div>
           </div>
-          <div className="col-md-6 col-lg-5 row align-items-center">
-            <WeatherInfo/>
+          <div className="row justify-content-center mt-5">
+            <Forecast />
           </div>
         </div>
-        <div className="row justify-content-center mt-5">
-          <Forecast />
-        </div>
-      </div>
+      )}
     </div>
   );
 }
--- src/components/TempDash.jsx
+++ src/components/TempDash.jsx
@@ -1,40 +1,26 @@
 import { useState, useEffect } from "react";
 
-export function TempDashInfo() {
-  const API_KEY = import.meta.env.VITE_API_KEY;
-  const [weatherData, setWeatherData] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=Edison&aqi=no`)
-      .then((response) => {
-        console.log(response.status);
-        return response.json()})
-      .then((data) => {
-        console.log(data);
-        setWeatherData(data);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-
+export function TempDashInfo({ weatherData }) {
+  // console.log(weatherData);
   return (
     <div>
-      {!isLoading && <div className="info-container">
-        <div className="location">{weatherData.location.name}, {weatherData.location.country}</div>
+      <div className="info-container">
+        <div className="location">
+          {weatherData.location.name}, {weatherData.location.country}
+        </div>
         <div className="date h5">{weatherData.location.localtime}</div>
         <div>
           <div className="temp m-0">{weatherData.current.temp_f}&deg;F</div>
           <div className="container">
             <div className="row w-50">
-              <div className="col-2 weather-description p-0 pt-1">{weatherData.current.condition.text}</div>
-              <img
-                className="weather-description-img col-1 img-fluid float-start p-0"
-                src={weatherData.current.condition.icon}
-                alt={weatherData.current.condition.text}
-              />
+              <div className="col d-flex ps-0">
+                <div className="weather-description p-0 pt-1">{weatherData.current.condition.text}</div>
+                <img
+                  className="weather-description-img img-fluid float-start p-0"
+                  src={weatherData.current.condition.icon}
+                  alt={weatherData.current.condition.text}
+                />
+              </div>
             </div>
             <div className="row">
               <div className="col-4 p-0">
@@ -46,15 +32,15 @@ export function TempDashInfo() {
             </div>
           </div>
         </div>
-      </div>}
+      </div>
     </div>
   );
 }
 
-export default function TempDash() {
+export default function TempDash({ weatherData }) {
   return (
     <>
-      <TempDashInfo />
+      <TempDashInfo weatherData={weatherData}/>
     </>
   );
 }
