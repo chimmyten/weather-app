3fefc84ee5a7e2816ac304396a21450e286b238a
Timmy Chen <chenx343@gmail.com>
2023-09-25 18:35:16 -0400

    implement timezone adjustment for forecast

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,21 +1,25 @@
 import rain from "../assets/rain.png";
 
 export default function Forecast({ weatherData, fahrenheit }) {
-  function getDate(dateTime) {
-    const dayOfWeek = dateTime.split(" ")[0];
-    const date = dateTime.split(" ").slice(1, 3).join(" ");
-    return `${dayOfWeek}, ${date}`;
-  }
-  //forecastday.date_epoch is in GMT, therefore take the localtime_epoch and add 24 hours to get next day and so on
-  const displayDate1 = getDate(new Date(weatherData.location.localtime_epoch * 1000 + 86400000).toString());
-  const displayDate2 = getDate(new Date(weatherData.location.localtime_epoch * 1000 + 86400000 * 2).toString());
-  const displayDate3 = getDate(new Date(weatherData.location.localtime_epoch * 1000 + 86400000 * 3).toString());
+  function getDisplayDate(dateString) {
+    const year = dateString.split("-")[0];
+    const month = dateString.split("-")[1];
+    const day = dateString.split("-")[2];
+    const date = new Date(year, month-1, day).toString();
 
+    const dayOfWeek = date.split(" ")[0];
+    const monthDay = date.split(" ").slice(1, 3).join(" ");
+    return `${dayOfWeek}, ${monthDay}`;
+  }
+
+  const day1 = getDisplayDate(weatherData.forecast.forecastday[1].date);
+  const day2 = getDisplayDate(weatherData.forecast.forecastday[2].date);
+  const day3 = getDisplayDate(weatherData.forecast.forecastday[3].date);
   return (
     <div className="container forecast overflow-hidden">
       <div className="row justify-content-center mb-3 forecast-heading">Forecast</div>
       <div className="row justify-content-center pb-2 gx-5">
-        <div className="col-3 col-lg-2 border-bottom">{displayDate1}</div>
+        <div className="col-3 col-lg-2 border-bottom">{day1}</div>
         <div className="col-3 col-lg-2 text-center border-bottom forecast-stat p-0">
           Lo:{" "}
           {fahrenheit
@@ -34,7 +38,7 @@ export default function Forecast({ weatherData, fahrenheit }) {
         </div>
       </div>
       <div className="row justify-content-center pb-2 gx-5">
-        <div className="col-3 col-lg-2 border-bottom">{displayDate2}</div>
+        <div className="col-3 col-lg-2 border-bottom">{day2}</div>
         <div className="col-3 col-lg-2 text-center border-bottom forecast-stat p-0">
         Lo:{" "}
           {fahrenheit
@@ -53,7 +57,7 @@ export default function Forecast({ weatherData, fahrenheit }) {
         </div>
       </div>
       <div className="row justify-content-center pb-2 gx-5">
-        <div className="col-3 col-lg-2 border-bottom">{displayDate3}</div>
+        <div className="col-3 col-lg-2 border-bottom">{day3}</div>
         <div className="col-3 col-lg-2 text-center border-bottom forecast-stat p-0">
         Lo:{" "}
           {fahrenheit
